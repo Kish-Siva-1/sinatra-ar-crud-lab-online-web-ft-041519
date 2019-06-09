3b50e77eff8d752339b4e22c02cc26ccceb8eaf1
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-09 20:51:04 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -45,6 +45,7 @@ class ApplicationController < Sinatra::Base
    
   delete '/articles/:id' do 
     @article = Article.find(params[:id])
+    binding.pry
     @article.destroy
     redirect to '/articles'
   end 
