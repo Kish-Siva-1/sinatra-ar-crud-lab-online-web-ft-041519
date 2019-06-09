71b782b329a243b310b16a5e3ffa1e35195c9003
Kishandth Sivapalasundaram <rodes88@msn.com>
2019-06-09 20:18:27 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -38,7 +38,6 @@ class ApplicationController < Sinatra::Base
   end
   
   patch '/articles/:id' do
-    binding.pry
     @article = Article.find(params[:id])
     @article[:title] = params[:title]
     @article[:content] = params[:content]
